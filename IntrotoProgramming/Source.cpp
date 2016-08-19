bae90fc22cd81ab5f7ecebd3432065d6c61d9804
BlaneTorregrossa <blanet97@cox.net>
2016-08-19 17:51:45 -0500

    Changes made during conditional statemnets excercise

--- IntrotoProgramming/Source.cpp
+++ IntrotoProgramming/Source.cpp
@@ -2,7 +2,46 @@
 
 int main()
 {
-	printf("hello you whover is reading this");
-	system("pause");
-		return 0;
+
+	//int num1;
+	//int num2;
+	//{
+		//if (num1 < num2)
+		//{
+			//std::cout << num2 << std::endl << "Is the higher" << std::endl << "Number!!!!!";
+			//std::cin.get();
+			//return 0;
+		//}
+		//else if (num1 > num2)
+		//{
+			//std::cout << num1 << std::endl << "Is the higher" << std::endl << "Number!!!!!!";
+			//std::cin.get();
+			//return 0;
+		//}
+		//else if (num1 == num2)
+		//{
+			//std::cout << "Either both numbers are the same or something unexpected has happened." << std::endl << "Number 1 and Number 2 will be displayed below" << std::endl << num1 << std::endl << num2;
+			//std::cin.get();
+			//return 0;
+		//}
+	//}
+
+
+
+	int i = 3;
+
+	if ((i == 1) + (i == 3))
+		if ((true) + (i == 3))
+
+			{
+				int fiveNumbers[5];
+				for (i = 0; i, 5; i++)
+					fiveNumbers[i] = i;	
+			}
+
+			
+	std::cout << i << std::endl;
+	std::cin.get();
+	return 0;
+
 }
