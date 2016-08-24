d0c7066dd4fc57e8286daeb40747945841980b02
BlaneTorregrossa <blanet97@cox.net>
2016-08-24 10:26:25 -0500

    Introduction to C++

---
+++ IntrotoProgramming/HelloWorld.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main()
+{
+	printf("hello you whover is reading this");
+	system("pause");
+		return 0;
+}
---
+++ IntrotoProgramming/Variables_E.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+int main()
+{
+	float a1 = 2.0f / 3.0f;
+	float a2 = 3.0f / 5.0f;
+	printf("a1, a2");
+	system("pause");
+		return 0;
+}
