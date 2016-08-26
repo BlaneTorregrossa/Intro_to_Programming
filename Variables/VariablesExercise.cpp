ef3dc1e08a0080e5edc3e3e20900a44f162a9780
BlaneTorregrossa <blanet97@cox.net>
2016-08-26 00:04:21 -0500

    Variables Excercise

---
+++ Variables/VariablesExercise.cpp
@@ -0,0 +1,98 @@
+//Exercise 1
+//#include <iostream>
+//int main()
+//{
+	//float a = 2.f / 3.f + 3.f / 5.f;
+	//float b = 2%3 + 3%5;
+	//float c = 23 % 15 % -2; //does not work as there is no remainder by the end so it just displays 0
+	//float d = 25.f * 1.f / 2.f;
+	//float e = 25.f * 1.0f / 2.f;
+	//float f = 25.f * (1.f / 2.f);
+	//float g = 235.f / 8.f + 7.f;
+	//float h = 235.f / 8.0f + 7.f;
+	//float i = ((20.f + 1.f) / 2.f - 2.0f) / (23.f + 3.f) * 0.2f;
+	//{
+	//	std::cout << "Exercise 1 answers in order" << std::endl << a << std::endl << b << std::endl << c << std::endl << d << std::endl << e << std::endl << f << std::endl << g << std::endl << h << std::endl << i;
+	//	std::cin.get();
+	//	return 0;
+	//}
+//}
+
+//Exercise 2
+//a.) int averageStudentHeights
+//b.) int taxRateHighIncome
+//c.) int taxRateLowIncome
+//d.) int personalSteamLibrayValue
+
+//Exercise 3
+//a.) 121 int interger
+//b.) 9.4 float floating varible
+//c.) r char character
+//d.) 100000 int interger
+//e.) true bool
+
+
+//Exercise 4
+//#include <iostream> 
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	int tmp;
+//	std::cin >> a >> b;
+//	std::cout << "a" << std::endl << a << std::endl << "b" << std::endl << b;
+//	std::cin.get();
+//	system("pause");
+	
+//	{
+//		tmp = a;
+//		a = b;
+//		b = tmp;
+		
+//	}
+//	std::cout << "a" << std::endl << a << std::endl << "b" << std::endl << b;
+//	std::cin.get();
+//	system("pause");
+//	return 0;
+//}
+
+//Exercise 5
+//#include <iostream>
+
+//int main()
+//{
+//	int i = 0;
+//	float f = 0;
+//	char ch = 'a';
+//	std::cin >> i >> ch >> f;
+//	std::cout << "i: " << i << "/nch: " << ch << "/nf: " << f << std::endl;
+//	system("pause");
+
+	//a.) i = 1 ch = A f = 45
+	//b.) i = 1 ch = A f = 45
+	//c.) i = 1 ch = 9 f = 45
+	//d.) i = 1 ch = 9 f = 45
+	//e.) i = 0 ch = a f = 0
+	//f.) i = 1 ch = B f = 0
+//}
+
+//Exercise 6
+//#include <iostream>
+//int main()
+//{
+//	int a;
+//	int b;
+//	int c;
+//	int d;
+//	int e;
+//	int avg;
+
+//	{
+//		std::cin >> a >> b >> c >> d >> e;
+//		avg = (a + b + c + d + e) / 5;
+//		std::cout << "The average is " << avg << std::endl;
+//		system("pause");
+//		return 0;
+//	}
+//}
+
