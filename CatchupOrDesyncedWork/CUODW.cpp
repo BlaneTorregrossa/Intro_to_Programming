9dfd20de2e85f83a4d9e4d1e70dfe6be57479614
BlaneTorregrossa <blanet97@cox.net>
2016-09-08 13:59:20 -0500

    Work with classes

--- CatchupOrDesyncedWork/CUODW.cpp
+++ CatchupOrDesyncedWork/CUODW.cpp
@@ -1,94 +1,51 @@
-//This is all a mess. Fix this when you get home.
 #include <iostream>
 
-struct Player
+class Student
 {
-	int turns = 0;
-	bool win;
+private:
+
+	char name = '0';
+	int age = 19;
+	int id = 553012;
+
+
+public:
+
+	char studentAName(char name);
+	int studentAAge(int age);
+	int studentAID(int id);
+
 };
 
-struct Opponent
+char Student::studentAName(char name)
 {
-	int turns = 0;
-	bool win;
-};
 
-int ttcmap[3][3]{ { 1, 2, 3},
-				{ 4, 5, 6},
-				{ 7, 8, 9} };
+	return 0;
+
+}
+
+int Student::studentAAge(int age)
+{
+	if (age == 19)
+	{
+		age = 23;
+	}
+	return 0;
+
+}
+
+int Student::studentAID(int id)
+{
+
+	return 0;
+}
+
+
 
 
 
 
 int main()
 {
-	char input = '0';
-	std::cout << "Test to see if things will work correctly." << std::endl;
-	system("pause");
-	system("cls");
-	std::cout << "Please input x" << std::endl;
-	std::cin >> input;
-	while (input != 'x')
-	{
-
-		//All of these if statements are wrong!
-		if (input == 'a')
-		{
-			std::cout << "top left" << std::endl;
-			std::cin >> input;
-		}
-		if (input == 'b')
-		{
-			std::cout << "top middle" << std::endl;
-			std::cin >> input;
-		}
-		if (input == 'c')
-		{
-			std::cout << "top right" << std::endl;
-			std::cin >> input;
-		}
-		if (input == 'd')
-		{
-			std::cout << "middle right" << std::endl;
-			std::cin >> input;
-		}
-		if (input == 'e')
-		{
-			std::cout << "true middle" << std::endl;
-			std::cin >> input;
-		}
-		if (input == 'f')
-		{
-			std::cout << "middle left" << std::endl;
-			std::cin >> input;
-		}
-		if (input == 'g')
-		{
-			std::cout << "bottom right" << std::endl;
-			std::cin >> input;
-		}
-		if (input == 'h')
-		{
-			std::cout << "bottom middle" << std::endl;
-			std::cin >> input;
-		}
-		if (input == 'i')
-		{
-			std::cout << "bottom right" << std::endl;
-			std::cin >> input;
-		}
-		else
-		{
-			std::cout << "All is wrong..." << std::endl;
-			system("pause");
-			break;
-		}
-		system("pause");
-	}
-	return 1;
-
-
-
-
-
+	
 }
--- Classes/ClassesExercise.cpp
+++ Classes/ClassesExercise.cpp
@@ -1,3 +1,31 @@
+//Classes Exercise 1
+
 #include <iostream>
 
-int main()
+class Zombie
+{
+
+
+
+private:
+
+	float zHealth, zdHealth;
+	float zAttack, zdAttack;
+	float zPreviousLife, zdPreviousLife;
+
+public:
+
+	void setZombieHealth(float zHealth)
+	{
+		if (zHealth == 100.0f)
+			zdHealth = 50.0f;
+		else
+			zdHealth = zHealth;
+	}
+
+};
+
+
+
+
+
