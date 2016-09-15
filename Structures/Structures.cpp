fe61067577cd50b1d62ce2af46f7abed648a59e0
BlaneTorregrossa <blanet97@cox.net>
2016-09-14 23:43:16 -0500

    Structures

---
+++ Structures/Structures.cpp
@@ -0,0 +1,26 @@
+//Structures 1
+//Description: Structure for a player's score, health, position, and velocity. Structure for a 2D rectangle where the position for four points are and it's color are determined.
+
+//struct Player
+//{
+//	char playerName;
+//	int playerHealth = 100;
+//	int playerScore = 0;
+//	float playerPositionX = 10.22;
+//	float playerPositionY = 3.75;
+//	float playerVelocity = 2.5;
+//};
+
+//struct Rectangle
+//{
+//	float pointAPosX = 65.221;
+//	float pointAPosY = 10.231;
+//	float pointBPosX = 50.221;
+//	float pointBPosY = 6.352;
+//	float pointCPosX = 25.981;
+//	float pointCPosY = 2.351;
+//	float pointDPosX = 58.261;
+//	float pointDPosY = 8.221;
+//	int color = 569064;
+//};
+
