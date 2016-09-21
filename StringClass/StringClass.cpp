821bb9f3d633325b26a63ae3c000f2495a5101ca
BlaneTorregrossa <blanet97@cox.net>
2016-09-21 17:28:17 -0500

    String Classes

--- StringClass/StringClass.cpp
+++ StringClass/StringClass.cpp
@@ -4,39 +4,81 @@
 #include <iostream>
 
 
-
+// how to properly get char information into the main function without problems
 int main()
 {
-	int i = 5;
-	StringClassA stringclassa = StringClassA(7, 8);
-	StringClassB stringclassb = StringClassB(8);
-	stringclassa.ArrayCount;
+	StringClassA stringclassa = StringClassA ('a', 'b');
+	StringClassB stringclassb = StringClassB('c', 'd');
+	
+
+	system("pause");
 	return 1;
 }
 
 StringClassA::StringClassA(char aa, char ab) //Function fors String class A
 {
-
 	ArrayA[7] = aa;
 	nameA[8] = ab;
+	nameADupA[8];
+	nameADupB[8];
 }
 
 
 
-StringClassB::StringClassB(char ba) //Function for string class B
+StringClassB::StringClassB(char ba, char bb) //Function for string class B
 {
-	nameB[8] = ba;
+
+	ArrayB[7] = ba;
+	nameB[8] = bb;
+	nameBDupA[8];
+	nameBDupB[8];
 }
 
 
-
-
-
-
-void StringClassA::ArrayCount() //Fuction for counting a strings length and returning a value
+void StringClassA::ArrayCount() //string length
 {
 	strlen(ArrayA);
 }
+
+
+void StringClassA::CharIndex() //acsessing character index
+{
+	nameA[8];
+}
+
+//Comparison of a string to see if it is the same as another string classs
+
+//Apenindng a string
+
+//Prepending a string
+
+//Reurning a string ias a basic constant C-style string
+
+
+void StringClassA::DuplicateLowerCase() // convert the string to a duplicate containng all lowercase letters
+{
+	strcpy_s(nameADupA, nameA);
+	if (strcmp(nameADupA, nameA) == 0)
+	{
+		strcpy_s(nameADupA, smallString);
+	}
+}
+
+void StringClassA::DuplicateUpperCase() // convert the string to a duplicate containing all uppercase letters
+{
+	strcpy_s(nameADupB, nameA);
+	if (strcmp(nameADupB, nameA) == 0)
+	{
+		strcpy_s(nameADupB, bigString);
+	}
+}
+
+
+// finding a sub-string within the string class starting from a certain index within the string
+
+// set the string to an input C-style string
+
+
 
 
 
@@ -47,27 +89,5 @@ void StringClassA::ArrayCount() //Fuction for counting a strings length and retu
 //I do not understand how to make a sub-string within the string class.
 //I am aware of functions string length, compare, and copy.
 
-
-//StringClassB appending itself to StringClassA
-StringClassB * StringClassA::findStringClassB()
-{
-	return nullptr;
-}
-
-
-
-//StringClassA appending itself to StringClassB
-StringClassA * StringClassB::findStringClassA()
-{
-	return nullptr;
-}
-
-
-
-//I do not understand how to get two classes to interact with each other for comparison.
-void StringClassA::StringCompare(int compareA)
-{
-	
-}
 
 
--- StringClass/StringClass.h
+++ StringClass/StringClass.h
@@ -4,38 +4,44 @@
 #define _StringClass_H_
 #include <iostream>
 
-//The first of two string classes
-//Contains an Array counter, two arrays, and a way to interact with StringClassB.
 class StringClassA
 {
 public:
 	StringClassA(char aa, char ab);
 	void ArrayCount();
-	void StringCompare(int compareA);
-	StringClassB* findStringClassB();
-	
-
+	void CharIndex();
+	void DuplicateLowerCase();
+	void DuplicateUpperCase();
 
 
 private:
-	StringClassB* B;
-	char ArrayA[7] = "sample";
-	char nameA[8] = "stringA";
+	
+	char ArrayA[7];
+	char nameA[8];
+	char nameADupA[8];
+	char nameADupB[8];
+	char smallString[8];
+	char bigString[8];
 };
 
 
 
-//The second string class
-//Contains a single Array and a way to interact with StringClassA
 class StringClassB
 {
 public:
-	StringClassB(char ba);
-	StringClassA* findStringClassA();
+	StringClassB(char ba, char bb);
+	
 
 private:
-	StringClassA* A;
+
+	char ArrayB[7] = "sample";
 	char nameB[8] = "StringB";
-	
+	char nameBDupA[8];
+	char nameBDupB[8];
+	char smallString[8] = "stringa";
+	char bigString[8] = "STRINGA";
 };
+
+
+
 #endif
